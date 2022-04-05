73568f33ba800791d6f292cfcc4b2bc679df18fc
Le Nhat Thanh <thanhdubai9944@gmail.com>
2022-04-05 16:12:02 +0700

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function (req, res, next) {
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+        cb(null, './uploads/')
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
