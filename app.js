4470b71de5881a6eefda5d704bdf8c6ff44e8fdc
Le Nhat Thanh <thanhdubai9944@gmail.com>
2022-04-05 16:08:01 +0700

    Initial commit

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'uploads')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
